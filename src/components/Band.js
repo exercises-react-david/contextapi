9b5d9a99301fa032dedae1ed9e261f59d7d0aa19
David Morales <davidmhdez22@gmail.com>
2019-05-07 13:20:41 -0600

    added styled components in Band.js because bootsrap did not apply the styles for the list

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import styled from 'styled-components';
 
 export default class Band extends Component {
 
@@ -6,14 +7,22 @@ export default class Band extends Component {
         const {name,songs} = this.props.band;
 
         return (
-           <list classname="list-group-item d-flex justify-content-between align-items-center">
+           <ListItem classname="list-group-item d-flex justify-content-between align-items-center">
                 <p className="m-0">
                     {name}
                 </p>
                 <span className="badge badge-danger">
                     {songs}
                 </span>
-           </list>
+           </ListItem>
         );
     }
 }
+
+const ListItem = styled.li`
+border: 1px solid gray;
+list-style: none;
+display: grid;
+grid-template-columns: 1fr 30px;
+padding: 5px;
+`;
