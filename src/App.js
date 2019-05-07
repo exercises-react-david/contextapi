0bf9212b0ecb925eae29e426dd6c63783389d69c
Aaron Koivunen <aaron@karbook.mx>
2019-05-07 11:11:42 -0500

    Make small changes to title and version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 
 export default class App extends Component {
-
-    render() {
-        return (
-            <div className="class-name">
-                content
-            </div>
-        );
-    }
+  render() {
+    return <div className="class-name">content goes here</div>;
+  }
 }
