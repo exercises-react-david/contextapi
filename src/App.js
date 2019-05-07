e8ee5ae7524de316fc97687d3a18e337bc130f35
Aaron Koivunen <aaron@karbook.mx>
2019-05-07 12:21:19 -0500

    Add map svg as a test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import map from "./usa-map.svg";
 
 export default class App extends Component {
   render() {
-    return <div className="class-name">content goes here</div>;
+    return (
+      <div>
+        <div className="class-name">content goes here</div>;
+        <img className="usa-map" src={map} />
+      </div>
+    );
   }
 }
